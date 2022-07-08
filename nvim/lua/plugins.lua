a70d621defc06ec87866b8340fbba35cccef2b56
Gustavo Verzola <verzola@gmail.com>
2022-07-08 18:39:39 -0300

    Tweak settings

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,9 +2,15 @@ vim.cmd "packadd packer.nvim"
 
 -- Plugins
 require('packer').startup(function()
-  use 'EdenEast/nightfox.nvim'
+  --use 'EdenEast/nightfox.nvim'
+  --use 'glepnir/dashboard-nvim'
+  --use 'rafi/awesome-vim-colorschemes'
+  --use 'rebelot/kanagawa.nvim'
+  --use 'voldikss/vim-floaterm'
+  use "pangloss/vim-javascript"
   use 'akinsho/bufferline.nvim'
   use 'catppuccin/nvim'
+  use 'dense-analysis/ale'
   use 'dracula/vim'
   use 'editorconfig/editorconfig-vim'
   use 'folke/tokyonight.nvim'
@@ -18,13 +24,11 @@ require('packer').startup(function()
   use 'mhinz/vim-startify'
   use 'mxw/vim-jsx'
   use 'neovim/nvim-lspconfig'
+  use 'norcalli/nvim-colorizer.lua'
   use 'nvim-lualine/lualine.nvim'
-  use { 'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} } }
   use 'nvim-treesitter/nvim-treesitter'
   use 'prettier/vim-prettier'
-  use 'rafi/awesome-vim-colorschemes'
   use 'rcarriga/nvim-notify'
-  use 'rebelot/kanagawa.nvim'
   use 'tpope/vim-commentary'
   use 'tpope/vim-fugitive'
   use 'tpope/vim-obsession'
@@ -32,12 +36,8 @@ require('packer').startup(function()
   use 'tpope/vim-sensible'
   use 'tpope/vim-surround'
   use 'vim-test/vim-test'
-  use 'voldikss/vim-floaterm'
   use 'wbthomason/packer.nvim'
-  use 'norcalli/nvim-colorizer.lua'
-  use 'dense-analysis/ale'
-  use "pangloss/vim-javascript"
-  --use 'glepnir/dashboard-nvim'
+  use { 'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} } }
 end)
 
 require('trouble').setup()
