d43784f5eff9ef91ca3691140185ba8af9f51b31
Gustavo Verzola <verzola@gmail.com>
2022-07-08 17:45:33 -0300

    Add more options

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,9 +1,20 @@
 vim.g.mapleader = ','
+vim.o.encoding = "utf-8"
 
 vim.cmd([[
   colorscheme catppuccin
+  set autoindent
+  set autoread
+  set background=dark
+  set backspace=indent,eol,start
+  set confirm
   set cursorline
   set expandtab
+  set formatoptions+=j
+  set hlsearch
+  set ignorecase
+  set incsearch
+  set lazyredraw
   set list
   set mouse=a
   set nobackup
@@ -12,21 +23,24 @@ vim.cmd([[
   set nowritebackup
   set number
   set pastetoggle=<F12>
+  set ruler
+  set shiftround
   set shiftwidth=2
   set showmatch
   set smartcase
   set smartindent
+  set smarttab
   set softtabstop=2
+  set spell
   set splitbelow
   set splitright
   set tabstop=2
   set termguicolors
+  set title
   set undodir=~/.vim-undo
   set undofile
   set undofile
   set undolevels=99999
-  set hlsearch
-  set ignorecase
-  set lazyredraw
   set updatetime=250
+  set wildmenu
 ]])
