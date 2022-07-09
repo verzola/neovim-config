03261b89d4cc4cb6a38834a6f941feb6b3bdbae0
Gustavo Verzola <verzola@gmail.com>
2022-07-09 16:30:47 -0300

    Configure wilder plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -152,6 +152,41 @@ require('packer').startup(function()
     end
   }
 
+  use {
+    'gelguy/wilder.nvim',
+    config = function()
+      local wilder = require('wilder')
+      wilder.setup({modes = {':', '/', '?'}})
+      wilder.set_option('renderer', wilder.renderer_mux({
+        [':'] = wilder.popupmenu_renderer({
+          highlighter = wilder.basic_highlighter(),
+        }),
+        ['/'] = wilder.wildmenu_renderer({
+          highlighter = wilder.basic_highlighter(),
+        }),
+        ['?'] = wilder.wildmenu_renderer({
+          highlighter = wilder.basic_highlighter(),
+        }),
+      }))
+      wilder.set_option('renderer', wilder.popupmenu_renderer({
+        pumblend = 20,
+      }))
+      wilder.set_option('renderer', wilder.popupmenu_renderer(
+        wilder.popupmenu_border_theme({
+          highlights = {
+            border = 'Normal',
+          },
+          border = 'rounded',
+        })
+      ))
+      wilder.set_option('renderer', wilder.popupmenu_renderer({
+        highlighter = wilder.basic_highlighter(),
+        left = {' ', wilder.popupmenu_devicons()},
+        right = {' ', wilder.popupmenu_scrollbar()},
+      }))
+    end,
+  }
+
   -- Terminal
   --use 'voldikss/vim-floaterm'
 end)
