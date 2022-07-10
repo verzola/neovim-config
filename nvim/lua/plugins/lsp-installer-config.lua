ec1842f5d98b867be19f666d5ee5e6bfee70c430
Gustavo Verzola <verzola@gmail.com>
2022-07-10 19:48:55 -0300

    Configure language servers

--- nvim/lua/plugins/lsp-installer-config.lua
+++ nvim/lua/plugins/lsp-installer-config.lua
@@ -1,2 +1,10 @@
-require('nvim-lsp-installer').setup { }
-require'lspconfig'.tsserver.setup { }
+require('nvim-lsp-installer').setup{}
+require'lspconfig'.html.setup{}
+require'lspconfig'.cssls.setup{}
+require'lspconfig'.cssmodules_ls.setup{}
+require'lspconfig'.tailwindcss.setup{}
+require'lspconfig'.tsserver.setup{}
+require'lspconfig'.jsonls.setup{}
+require'lspconfig'.bashls.setup{}
+require'lspconfig'.dockerls.setup{}
+require'lspconfig'.ansiblels.setup{}
---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+sudo apt install -y xsel
+sudo npm install -g neovim
+sudo npm install -g tree-sitter
+sudo npm install -g vscode-langservers-extracted
+sudo npm install -g cssmodules-language-server
+sudo npm install -g @tailwindcss/language-server
+sudo npm install -g bash-language-server
+sudo npm install -g dockerfile-language-server-nodejs
+sudo npm install -g @ansible/ansible-language-server
+pip install pynvim
