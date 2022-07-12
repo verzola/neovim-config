e21050cfffc8ed64a3860b4028febfea41e7d1ef
Gustavo Verzola <verzola@gmail.com>
2022-07-11 22:36:22 -0300

    Enable ghost text nvim-cmp experimental feature

--- nvim/lua/plugins/cmp-config.lua
+++ nvim/lua/plugins/cmp-config.lua
@@ -1,6 +1,9 @@
 local cmp = require'cmp'
 
 cmp.setup({
+  experimental = {
+    ghost_text = true,
+  },
   snippet = {
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
@@ -17,11 +20,12 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<c-space>'] = cmp.mapping.complete(),
+    ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
   sources = cmp.config.sources({
+    { name = 'nvim_lua' },
     { name = 'nvim_lsp' },
     { name = 'vsnip' }, -- For vsnip users.
     -- { name = 'luasnip' }, -- For luasnip users.
