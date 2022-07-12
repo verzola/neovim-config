27098717df89f227f099f1af2c08f921507905c0
Gustavo Verzola <verzola@gmail.com>
2022-07-11 21:37:23 -0300

    Add config to auto install lsp servers

--- nvim/lua/plugins/lsp-installer-config.lua
+++ nvim/lua/plugins/lsp-installer-config.lua
@@ -1,12 +1,30 @@
-require('nvim-lsp-installer').setup{}
-require'lspconfig'.html.setup{}
-require'lspconfig'.cssls.setup{}
-require'lspconfig'.cssmodules_ls.setup{}
-require'lspconfig'.tailwindcss.setup{}
-require'lspconfig'.intelephense.setup{}
-require'lspconfig'.tsserver.setup{}
-require'lspconfig'.jsonls.setup{}
-require'lspconfig'.bashls.setup{}
-require'lspconfig'.dockerls.setup{}
-require'lspconfig'.ansiblels.setup{}
-require'lspconfig'.sumneko_lua.setup{}
+require('nvim-lsp-installer').setup{
+  ensure_installed = {
+     'ansiblels',
+     'bashls',
+     'cssls',
+     'cssmodules_ls',
+     'dockerls',
+     'emmet_ls',
+     'eslint',
+     'html',
+     'intelephense',
+     'jsonls',
+     'sumneko_lua',
+     'tailwindcss',
+     'tsserver',
+     'yamlls',
+  },
+  automatic_installation = true,
+}
+-- require'lspconfig'.html.setup{}
+-- require'lspconfig'.cssls.setup{}
+-- require'lspconfig'.cssmodules_ls.setup{}
+-- require'lspconfig'.tailwindcss.setup{}
+-- require'lspconfig'.intelephense.setup{}
+-- require'lspconfig'.tsserver.setup{}
+-- require'lspconfig'.jsonls.setup{}
+-- require'lspconfig'.bashls.setup{}
+-- require'lspconfig'.dockerls.setup{}
+-- require'lspconfig'.ansiblels.setup{}
+-- require'lspconfig'.sumneko_lua.setup{}
