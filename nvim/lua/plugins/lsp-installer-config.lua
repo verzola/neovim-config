91188a5391d2894145029087f872b6d75894d47b
Gustavo Verzola <verzola@gmail.com>
2022-07-11 21:39:52 -0300

    Uncomment lsp config

--- nvim/lua/plugins/lsp-installer-config.lua
+++ nvim/lua/plugins/lsp-installer-config.lua
@@ -1,3 +1,15 @@
+require'lspconfig'.html.setup{}
+require'lspconfig'.cssls.setup{}
+require'lspconfig'.cssmodules_ls.setup{}
+require'lspconfig'.tailwindcss.setup{}
+require'lspconfig'.intelephense.setup{}
+require'lspconfig'.tsserver.setup{}
+require'lspconfig'.jsonls.setup{}
+require'lspconfig'.bashls.setup{}
+require'lspconfig'.dockerls.setup{}
+require'lspconfig'.ansiblels.setup{}
+require'lspconfig'.sumneko_lua.setup{}
+
 require('nvim-lsp-installer').setup{
   ensure_installed = {
      'ansiblels',
@@ -17,14 +29,3 @@ require('nvim-lsp-installer').setup{
   },
   automatic_installation = true,
 }
--- require'lspconfig'.html.setup{}
--- require'lspconfig'.cssls.setup{}
--- require'lspconfig'.cssmodules_ls.setup{}
--- require'lspconfig'.tailwindcss.setup{}
--- require'lspconfig'.intelephense.setup{}
--- require'lspconfig'.tsserver.setup{}
--- require'lspconfig'.jsonls.setup{}
--- require'lspconfig'.bashls.setup{}
--- require'lspconfig'.dockerls.setup{}
--- require'lspconfig'.ansiblels.setup{}
--- require'lspconfig'.sumneko_lua.setup{}
