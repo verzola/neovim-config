af1b59415619d93fd6be0cea1f4140788535d2f7
Gustavo Verzola <verzola@gmail.com>
2022-07-11 22:02:25 -0300

    Configure nvim-cmp

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -118,7 +118,11 @@ return packer.startup(function()
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
   use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/nvim-cmp'
+  use {
+    'hrsh7th/nvim-cmp', config = function()
+      require 'plugins.cmp-config'
+    end
+  }
 
   -- Formatter & Linter
   use 'dense-analysis/ale'
--- nvim/lua/plugins/lsp-installer-config.lua
+++ nvim/lua/plugins/lsp-installer-config.lua
@@ -1,18 +1,3 @@
-require'lspconfig'.ansiblels.setup{}
-require'lspconfig'.bashls.setup{}
-require'lspconfig'.cssls.setup{}
-require'lspconfig'.cssmodules_ls.setup{}
-require'lspconfig'.dockerls.setup{}
-require'lspconfig'.emmet_ls.setup{}
-require'lspconfig'.eslint.setup{}
-require'lspconfig'.html.setup{}
-require'lspconfig'.intelephense.setup{}
-require'lspconfig'.jsonls.setup{}
-require'lspconfig'.sumneko_lua.setup{}
-require'lspconfig'.tailwindcss.setup{}
-require'lspconfig'.tsserver.setup{}
-require'lspconfig'.yamlls.setup{}
-
 require('nvim-lsp-installer').setup{
   ensure_installed = {
      'ansiblels',
